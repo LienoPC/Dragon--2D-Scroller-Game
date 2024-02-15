57f6815baf985a58a205b93f913c4960765b1bcb
giulio-arecco <giulioarecco@gmail.com>
2024-02-15 12:48:49 +0100

    Fix: sistemato e alleggerito l'aggiornamento delle hitbox del drago col movimento

--- OpenGLApp/game_structures/dragon.cpp
+++ OpenGLApp/game_structures/dragon.cpp
@@ -55,30 +55,12 @@ void Dragon::move(glm::vec2 move) {
 	// Move the dragon
 	this->position += move;
 	// Move the hitboxes with the dragon
-	this->hitboxes[0].left_up = glm::vec2(this->position.x + 45 * size.x / 800, this->position.y + 290 * size.y / 800);
-	this->hitboxes[0].left_down = glm::vec2(this->position.x + 45 * size.x / 800, this->position.y + 475 * size.y / 800);
-	this->hitboxes[0].right_up = glm::vec2(this->position.x + 250 * size.x / 800, this->position.y + 290 * size.y / 800);
-	this->hitboxes[0].right_down = glm::vec2(this->position.x + 250 * size.x / 800, this->position.y + 475 * size.y / 800);
-
-	this->hitboxes[1].left_up = glm::vec2(this->position.x + 250 * size.x / 800, this->position.y + 350 * size.y / 800);
-	this->hitboxes[1].left_down = glm::vec2(this->position.x + 250 * size.x / 800, this->position.y + 465 * size.y / 800);
-	this->hitboxes[1].right_up = glm::vec2(this->position.x + 385 * size.x / 800, this->position.y + 350 * size.y / 800);
-	this->hitboxes[1].right_down = glm::vec2(this->position.x + 385 * size.x / 800, this->position.y + 465 * size.y / 800);
-
-	this->hitboxes[2].left_up = glm::vec2(this->position.x + 385 * size.x / 800, this->position.y + 270 * size.y / 800);
-	this->hitboxes[2].left_down = glm::vec2(this->position.x + 385 * size.x / 800, this->position.y + 690 * size.y / 800);
-	this->hitboxes[2].right_up = glm::vec2(this->position.x + 415 * size.x / 800, this->position.y + 270 * size.y / 800);
-	this->hitboxes[2].right_down = glm::vec2(this->position.x + 415 * size.x / 800, this->position.y + 690 * size.y / 800);
-
-	this->hitboxes[3].left_up = glm::vec2(this->position.x + 415 * size.x / 800, this->position.y + 350 * size.y / 800);
-	this->hitboxes[3].left_down = glm::vec2(this->position.x + 415 * size.x / 800, this->position.y + 465 * size.y / 800);
-	this->hitboxes[3].right_up = glm::vec2(this->position.x + 550 * size.x / 800, this->position.y + 350 * size.y / 800);
-	this->hitboxes[3].right_down = glm::vec2(this->position.x + 550 * size.x / 800, this->position.y + 465 * size.y / 800);
-
-	this->hitboxes[4].left_up = glm::vec2(this->position.x + 550 * size.x / 800, this->position.y + 290 * size.y / 800);
-	this->hitboxes[4].left_down = glm::vec2(this->position.x + 550 * size.x / 800, this->position.y + 475 * size.y / 800);
-	this->hitboxes[4].right_up = glm::vec2(this->position.x + 755 * size.x / 800, this->position.y + 290 * size.y / 800);
-	this->hitboxes[4].right_down = glm::vec2(this->position.x + 755 * size.x / 800, this->position.y + 475 * size.y / 800);
+	for (int i = 0; i < this->hitboxes.size(); i++) {
+		this->hitboxes[i].left_up += move;
+		this->hitboxes[i].left_down += move;
+		this->hitboxes[i].right_up += move;
+		this->hitboxes[i].right_down += move;
+	}
 }
 
 void Dragon::Draw(SpriteRenderer& renderer, float dt) {
